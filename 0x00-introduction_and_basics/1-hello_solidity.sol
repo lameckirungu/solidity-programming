bf81eb1084f2619df0f8f53de77866893ab20789
Lameck Irungu <lameckirungu90@gmail.com>
2024-10-07 18:28:17 +0300

    Implement HelloSolidity contract with sayHello() function (Task 1)

---
+++ 0x00-introduction_and_basics/1-hello_solidity.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier
+
+pragma solidity ^0.8.0;
+
+/**
+ * HelloSolidity - the contract
+ * sayHello - prints a string
+ */
+
+contract HelloSolidity {
+	function sayHello() public view returns (string memory) {
+		returns "Hello, Solidity!";
+	}
+}
+
